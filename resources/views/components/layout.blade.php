12e9bf465ee148e9decaad93aaf7bf45190b2269
Karim Tarek <karimabougom3@gmail.com>
2022-10-26 15:23:31 +0200

    First Commit

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<title>my Blog</title>
+<link rel="stylesheet" href="/app.css">
+
+<body>
+    {{ $slot }}
+
+</body>
+
---
+++ resources/views/posts.blade.php
@@ -0,0 +1,14 @@
+<x-layout>
+
+        @foreach ($posts as $post)
+        <article class="{{ $loop->even ? 'mb-4' : '' }}">
+            <h1>
+                <a href="/posts/{{ $post->slug }}">
+                    {{ $post->title }}</a>
+            </h1>
+            <p>{!! $post->body !!}</p>
+        </article>
+        @endforeach
+    
+</x-layout>
+
