17e0ea5c831c6d9bce696c8bd8dd8708582e88e4
Karim Tarek <karimabougom3@gmail.com>
2022-10-26 17:30:43 +0200

    Create Post Module

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -3,12 +3,12 @@
         @foreach ($posts as $post)
         <article class="{{ $loop->even ? 'mb-4' : '' }}">
             <h1>
-                <a href="/posts/{{ $post->slug }}">
+                <a href="/posts/{{ $post->id }}">
                     {{ $post->title }}</a>
             </h1>
             <p>{!! $post->body !!}</p>
         </article>
         @endforeach
-    
+
 </x-layout>
 
