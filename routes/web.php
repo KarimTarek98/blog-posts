c51e076d2889152887673a2d9f5cf3cc1d561cdf
Karim Tarek <karimabougom3@gmail.com>
2022-10-28 14:07:54 +0200

    Make some components

--- routes/web.php
+++ routes/web.php
@@ -21,10 +21,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
 
     return view('posts', [
-        'posts' => Post::latest()->with(['category', 'author'])->get()
+        'posts' => Post::latest()->with(['category', 'author'])->get(),
+        'categories' => Category::all()
     ]);
 
-});
+})->name('home');
 
 Route::get('/posts/{post:slug}', function (Post $post) {
     //$post = Post::findOrFail($id);
@@ -38,12 +39,15 @@ Route::get('/posts/{post:slug}', function (Post $post) {
 
 Route::get('/categories/{category:slug}', function (Category $category) {
     return view('posts', [
-        'posts' => $category->posts->load(['category', 'author'])
+        'posts' => $category->posts->load(['category', 'author']),
+        'activeCategory' => $category,
+        'categories' => Category::all()
     ]);
 });
 
 Route::get('/authors/{author}', function (User $author) {
     return view('posts', [
-        'posts' => $author->posts->load(['category', 'author'])
+        'posts' => $author->posts->load(['category', 'author']),
+        'categories' => Category::all()
     ]);
 });
